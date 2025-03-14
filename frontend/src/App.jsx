778d650693c481dfe9e4d575d0bdd9ca6e262913
Nikko <nikkonuriel@gmail.com>
2025-03-15 03:07:57 +0800

    initial commit for new journey

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Food from './components/Food.jsx'
+
+function App() {
+  
+    return (
+      <>
+      <Header></Header>
+      <Food></Food>
+      <Footer></Footer>
+      </>
+    );
+}
+
+export default App
